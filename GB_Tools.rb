886a3443da06ab36318cf0658b54f4a1380c7f8d
Matthew Gordon <matt.gordon320@gmail.com>
2013-10-29 16:21:24 -0700

    Return to simple tests

---
+++ GB_Tools.rb
@@ -0,0 +1,44 @@
+=begin
+	
+comments go here
+	
+=end
+
+
+require "sketchup.rb"
+
+module GB_Tools
+
+	unless file_loaded? (__FILE__)
+		menu = UI.menu('Plugins')
+		menu.add_item('GB_Tools') {self.message_test}
+	end
+
+	#Use instance variables inside modules instead of global variables.
+	@test_variable = 'Graphic Bridge Tool Test!'
+
+	# def self.message_test
+	# 	puts @test_variable
+	# end
+
+	def self.message_test
+		puts UI.messagebox "#{@test_variable}", MB_YESNOCANCEL
+		if result = 6 # YES
+			UI.messagebox("You clicked YES!")
+		elsif result = 7 #NO
+			UI.messagebox("You clicked NO!")
+		end
+	end #def
+
+
+	file_loaded(__FILE__)
+
+	# ### PREFERENCES ###-----------------------------------------
+
+	# ### MENU & TOOLBARS ###-------------------------------------
+
+	# # # MENUS
+	# m = GB_Tools.menu('GB_Tools').add_submenu('Guide Lines')
+	# m.add_item(example)
+
+end #Module
